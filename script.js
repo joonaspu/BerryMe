7a695b2b63b8a4965d0d3104c69a3fa883a817d0
Joonas Pussinen <joopu@student.uef.fi>
2018-11-12 22:45:51 +0200

    Make berry list scrollable and add some testing berries

--- script.js
+++ script.js
@@ -375,7 +375,11 @@ function generateBerries() {
     g_berries = [{
             "nab":{"name":"NotABerry", "url":"res/questionmark.png"},
             "blueberry":{ "name": "Blueberry", "url": "res/blueberry.png"},
-            "lingonberry":{"name": "Lingonberry","url":"res/lingonberry.png"}
+            "lingonberry":{"name": "Lingonberry","url":"res/lingonberry.png"},
+            "testberry1":{"name": "testberry1","url":"res/lingonberry.png"},
+            "testberry2":{"name": "testberry2","url":"res/lingonberry.png"},
+            "testberry3":{"name": "testberry3","url":"res/lingonberry.png"},
+            "testberry4":{"name": "testberry4","url":"res/lingonberry.png"},
             }];
 }
 
