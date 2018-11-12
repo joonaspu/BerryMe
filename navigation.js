73962bd962f359d9973965ba935d17f531194e7d
Antti Neuvonen <anttine@student.uef.fi>
2018-11-12 15:06:29 +0200

    Add download map

--- navigation.js
+++ navigation.js
@@ -7,9 +7,22 @@ $(document).on("shown.bs.modal","#myMaps",function(event) {
     let html = "";
     for(let i = 0;i<mapnames.length;i++) {
         let mapname = mapnames[i];
-        let el = `<li class="list-group-item"><button type="button" class="btn" onClick="changeMap('${mapname}')">${mapname}</button></li>`;
+        let el = `<li class="list-group-item">
+                        <button type="button" class="btn" onClick="changeMap('${mapname}')">${mapname}</button>
+                        <button type="button" class="btn" onClick="downloadMap('${mapname}')">Download</button>
+                    </li>`;
         html += el;
     } 
     document.getElementById("mapList").innerHTML = html;
 });
+
+//TODO: What we need to save
+function downloadMap(mapname) {
+    let data = new Blob([JSON.stringify(loadData(mapname))],{type: "text/plain"});
+    console.log("Trying to download");
+    let anchor = document.createElement("a");
+    anchor.download = mapname+".txt";
+    anchor.href = window.URL.createObjectURL(data)
+    anchor.click();
+}
 
