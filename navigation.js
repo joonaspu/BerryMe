5d95d008ce4596c5377e7401c1bd1e3d75c2c1d1
Antti Neuvonen <anttine@student.uef.fi>
2018-11-16 21:47:56 +0200

    Add rename window

--- navigation.js
+++ navigation.js
@@ -33,30 +33,47 @@ function newMapButtonClick() {
 // TODO: Success confirmation
 function openRemoveWindow(mapname) {
 
-    let el = `  <h2>Remove map ${mapname} permanently?</h2>
-                <button type="button" class="btn yesButton">Yes</button>
-                <button type="button" class="btn noButton">No</button>`;
-
+    let el = `  <h2 class="text-light">Remove: "${mapname}" permanently?</h2>
+                <button type="button" class="btn btn-lg btn-block btn-success yesButton">Yes <i class="fas fa-trash-alt"></i></button>
+                <br>
+                <button type="button" class="btn btn-lg btn-block btn-danger noButton">No</button>`;
+                
     document.getElementById("genericWindowContent").innerHTML = el;
+
     document.getElementById("genericWindowContent").querySelector(".yesButton").addEventListener("click", function() {
         removeMap(mapname);
         console.log("Removed map: "+mapname);
         $("#genericWindow").modal("hide");
-        $("#myMaps").modal("hide");
     });
     document.getElementById("genericWindowContent").querySelector(".noButton").addEventListener("click", function() {
         $("#genericWindow").modal("hide");
     });
-    $("#genericWindow").modal("show");
 
-    
-    
+    $("#genericWindow").modal("show");
+  
 }
 
 //TODO: make window
 function openRenameWindow(mapname) {
-    renameMap(mapname,"Renamed map");
-    $("#myMaps").modal("hide");
+    let el = `  <h2 class="text-light">Rename the map: ${mapname}</h2>
+                <form class="form-inline my-2 my-lg-0">
+                    <button class="btn mr-sm-2 renameButton" type="button">Rename</button>
+                    <br>
+                    <input class="form-control my-2 my-sm-0" placeholder="New Name" id="rename-map-input">                           
+                </form>`;
+    document.getElementById("genericWindowContent").innerHTML = el;
+
+    document.getElementById("genericWindowContent").querySelector(".renameButton").addEventListener("click", function() {
+        let name = document.getElementById("genericWindowContent").querySelector("#rename-map-input").value;
+        if(!name.length > 0) {
+            return;
+        }
+        renameMap(mapname,name);
+        $("#genericWindow").modal("hide");
+        $("#myMaps").modal("hide");
+    });
+    $("#genericWindow").modal("show");
+    
 }
 
 
