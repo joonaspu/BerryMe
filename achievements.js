fc15cf76830b306c7dbeb2484d4f42257e6349de
Antti Neuvonen <anttine@student.uef.fi>
2018-11-21 18:57:08 +0200

    Modify achievements

--- achievements.js
+++ achievements.js
@@ -1,25 +1,30 @@
 // Handle achievements
-// Format in localstorage -> e.g. [0,0,1,0,1]
+
+// Format in localstorage -> e.g. [id,id,...]
 achievements = [
-    {"name":"It's a beginning", "description":"Add one berry", "url":"res/achievements/radu.png"},
-    {"name":"Test2", "description":"Test description2", "url":"res/achievements/radu.png"},
-    {"name":"Test3", "description":"Test description2 asdasd", "url":"res/achievements/radu.png"},
-    {"name":"Test4", "description":"Test description2 asdasdasdasda", "url":"res/achievements/radu.png"},
-    {"name":"Test5", "description":"Test description2 ddddddddddddddddd", "url":"res/achievements/radu.png"},
-    {"name":"Test6", "description":"Test description2 asd", "url":"res/achievements/radu.png"},
-    {"name":"Test7", "description":"Test description2 ääöäöäö", "url":"res/achievements/radu.png"},
+    {"id": 0,"name":"Beginner", "description":"Add 1 berry", "url":"res/achievements/radu.png"},
+    {"id": 1,"name":"Novice", "description":"Add 25 berries", "url":"res/achievements/radu.png"},
+    {"id": 2,"name":"Professional", "description":"Add 50 berries", "url":"res/achievements/radu.png"},
+    {"id": 3,"name":"Veteran", "description":"Add 100 berries", "url":"res/achievements/radu.png"},
+    {"id": 4,"name":"What are you doing here?", "description":"Visit Tuusniemi", "url":"res/achievements/radu.png"},
+    {"id": 5,"name":"Joe's mouth", "description":"Visit Joensuu", "url":"res/achievements/radu.png"},
+    {"id": 6,"name":"Pasanen", "description":"Visit Kuopio", "url":"res/achievements/radu.png"},
 ];
 
-function initAchievements() {
-
-}
-
+// Load unlocked achievements from the localStorage
 function getAchievements() {
     let unlocked = window.localStorage.getItem("unlockedAchievements");
+    // Create key: "unlockedAchievements" if it does not exist
     if(!unlocked) {
-        //window.localStorage.setItem(unlockedAchievements,)
+        window.localStorage.setItem(unlockedAchievements, JSON.stringify([]));
     }
     unlocked = JSON.parse(unlocked);
-
     return unlocked;
+}
+
+// Unlock an achievement and save it 
+function unlockAchievement(id) {
+    let ach = getAchievements();
+    ach.push(id);
+    window.localStorage.setItem(unlockedAchievements, JSON.stringify(ach));
 }
